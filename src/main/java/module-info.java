28986a1d426d6cf0704383971ee5e235b9c96c84
romsgrlt <107188360+romsgrlt@users.noreply.github.com>
2023-04-11 17:03:34 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,4 @@
 module com.romsteam.clicker {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires net.synedra.validatorfx;
     requires lombok;
     requires com.fasterxml.jackson.core;
-
-    opens com.romsteam.clicker to javafx.fxml;
-    exports com.romsteam.clicker;
 }
